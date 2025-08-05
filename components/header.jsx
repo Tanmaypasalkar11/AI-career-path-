5010bff2a1564811bea2eaab275cc482722d7f0b
Tanmay <pasalkar8899@gmail.com>
2025-08-05 22:10:02 +0530

    install all things

---
+++ components/header.jsx
@@ -0,0 +1,23 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignIn,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
+};
+
+export default Header;
